53f255ca4afd337355dc168c602c34aed389c100
Yamori-zen <jersonmorocho16@gmail.com>
2018-12-07 12:02:33 -0500

    Se corrigio la visualización en iPhone 5/5S

--- resources/js/cont.js
+++
@@ -1,8 +0,0 @@
-  $(document).ready(function() {
-    $('.countdown-clock').TimeCircles({
-      time:{
-        Days:{color:"#36B3ED"},Hours:{color:"#36B3ED"},
-        Minutes:{color:"#36B3ED"},Seconds:{color:"#36B3ED"}
-      },
-    })
-});
