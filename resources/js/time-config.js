b45fbc4e66e15ee860860c87d7511037db94beef
Yamori-zen <jersonmorocho16@gmail.com>
2018-12-08 18:17:21 -0500

    Minificación de css y js

--- resources/js/time-config.js
+++ resources/js/time-config.js
@@ -1,9 +1,1 @@
-$(document).ready(function () {
-  $('.countdown-clock').TimeCircles({
-    circle_bg_color: "#fff",
-    time: {
-      Days: { color: "#0fc66a" }, Hours: { color: "#0fc66a" },
-      Minutes: { color: "#0fc66a" }, Seconds: { color: "#0fc66a" }
-    },
-  })
-});
+$(document).ready(function(){$(".countdown-clock").TimeCircles({circle_bg_color:"#fff",time:{Days:{color:"#0fc66a"},Hours:{color:"#0fc66a"},Minutes:{color:"#0fc66a"},Seconds:{color:"#0fc66a"}}})});
